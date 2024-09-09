5e04bb36c00f5585f176f508070bf7cd985861c8
Mehran <mehransurface@outlook.com>
2024-09-09 16:07:00 +0330

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace kish_insurance_service
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
